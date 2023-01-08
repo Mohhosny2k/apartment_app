1e8e8d245daeac8669d12b9d196c32f66c940928
Mohamed Hosny <92595555+Mohhosny2k@users.noreply.github.com>
2023-01-09 00:40:03 +0200

    Delete demo.dart

--- lib/demo.dart
+++
@@ -1,9 +0,0 @@
-// gmail email => ha6198355@gmail.com
-// gmail password => HassanAli123###
-//
-
-/*
-Certificate fingerprints:
-         SHA1: 0E:7A:47:D0:5A:0F:28:E0:5A:A2:EC:8C:22:2D:C1:EB:DE:76:2F:20
-         SHA256: E9:5B:3D:48:17:21:6D:1E:05:3F:8B:D8:8B:12:7E:38:0D:E0:EC:54:B3:65:C7:CC:5C:3B:76:0C:D1:41:DC:6C
-*/
